d866736ab6c083ea04f27f0092574a4994fea6aa
Lenawesome <nawilldominate@gmail.com>
2019-07-26 15:55:54 +0700

    Test block with customerrepositoryinterface

--- PracCus/Block/ChangePassword.php
+++ PracCus/Block/ChangePassword.php
@@ -3,16 +3,27 @@ namespace ABC\PracCus\Block;
 class ChangePassword extends \Magento\Framework\View\Element\Template
 {
     protected $registry;
+    protected $customerFactory;
+    protected $customerRepository;
     public function __construct(
     \Magento\Framework\View\Element\Template\Context $context,
-    \Magento\Framework\Registry $registry
+    \Magento\Framework\Registry $registry,
+    \Magento\Customer\Model\CustomerFactory $customerFactory,
+    \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository
     )
     {
         $this->registry = $registry;
-        parent::__construct($context);
+        $this->customerFactory = $customerFactory;
+        $this->customerRepository = $customerRepository;
+        return parent::__construct($context);
     }
+   
     public function getCustomerId(){
         $customerId = $this->registry->registry('customer_id');
         return $customerId;
+    }
+     public function getCustomer(){
+        $customer = $customerRepository->getById($this->getCustomerId());
+        return $customer;
     }
 }
